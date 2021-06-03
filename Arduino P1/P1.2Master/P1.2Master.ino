f077b9c8c23e81b94e38d283a8901b27fbd509c6
jadhasfuh <64806015+jadhasfuh@users.noreply.github.com>
2021-06-03 12:49:25 -0500

    CHAPUZA
    ABUZA

--- Arduino P1/P1.2Master/P1.2Master.ino
+++ Arduino P1/P1.2Master/P1.2Master.ino
@@ -1,56 +1,32 @@
+#include <Wire.h>
+
 //CEJA RENTERIA ADRIAN 17420533
 //IXTA ZAMUDIO LUIS JOSE 17420533
 //MEDIOLA CORREA CESAR PAULINO 17420533
 
 //MASTER PRACTICA 1.2
-#include <Wire.h>
 
-byte M = 0;
+char m = 'E';
 
-void setup(){
-
-  //INICIALIZAMOS COMUNICACION
-  Serial.begin(9600);
-  //INICIALIZAMOS BUS COMO MASTER SIN DIRECCION
-  Wire.begin();   
-  //INICIALIZAMOS LOS COMPONTES
-  pinMode(13, OUTPUT);
-           
+void setup() {
+  Wire.begin(); 
+  pinMode(13,OUTPUT);
 }
 
-void loop(){
+void loop() { 
 
-  //INICIAMOS TRANMISION 
-  Wire.beginTransmission(0x08);
-  
-  //ENVIA AL ESCLAVO
-  if (M == 1){
-    M = 0;
-    Wire.write(M);
-  }else if (M == 0) {
-    M = 1;
-    Wire.write(M);
+  if (m == 'E'){
+    digitalWrite(13, HIGH);
+    m = 'A';
+  } else {
+    digitalWrite(13, LOW);
+    m = 'E';
   }
-  Serial.print("MASTER ENVIA: ");
-  Serial.println(M);
-  
-  delay(100);
-  
-  //TERMINAMOS TRANMISION 
-  Wire.endTransmission(); 
 
-  //PEDIMOS INFO
-  Wire.requestFrom(0x08, 1);
-
-  //LEE AL ESCLAVO
-  while(Wire.available()) M = Wire.read();
-   
-  Serial.print("MASTER RECIVE: ");
-  Serial.println(M);
+  Wire.beginTransmission(9); 
+  Wire.write(m);              
+  Wire.endTransmission();  
   
-  if (M == 1) digitalWrite(13,HIGH);
-  else if (M == 0) digitalWrite(13,LOW);
-
   delay(1000);
   
 }
--- Arduino P1/P1.2Slave/P1.2Slave.ino
+++ Arduino P1/P1.2Slave/P1.2Slave.ino
@@ -1,59 +1,32 @@
+#include <Wire.h>
+
 //CEJA RENTERIA ADRIAN 17420533
 //IXTA ZAMUDIO LUIS JOSE 17420533
 //MEDIOLA CORREA CESAR PAULINO 17420533
 
 //SLAVE PRACTICA 1.2
-#include <Wire.h>
 
-byte M = 0;
+char m;
 
-void setup(){
-
-  //INICIALIZAMOS COMUNICACION
-  //Serial.begin(9600);
-  //INICIALIZAMOS BUS COMO ESCLAVO
-  Wire.begin(0x08); 
-  //FUNCION QUE SE ACTIVA CUANDO ALGO SE RECIVE
-  Wire.onReceive(solicitud);
-  //FUNCION QUE SE ACTIVA CUANDO ALGO SE PIDE
-  Wire.onRequest(pedido); 
-  //INICIALIZAMOS LOS COMPONTES
-  pinMode(13, OUTPUT);
-      
+void setup() {
+  pinMode (13, OUTPUT);
+  Wire.begin(9); 
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
 }
 
-void loop(){
-
-  luz();
-  delay (1000);
-  
+void receiveEvent(int bytes) {
+  m = Wire.read();  
 }
 
-void solicitud() {
-  
-  //LEE AL MAESTRO
-  while(Wire.available()) M = Wire.read();
-  //Serial.print("SLAVE RECIBE: ");
-  //Serial.println(M);
-  
+void requestEvent(){
+  if (m == 'E') m = 'A';
+  else m == 'E';
+  Wire.write(m);
 }
 
-void pedido() {
-  
-  //ENVIA AL MAESTRO
-  if (M == 1){
-    M = 0;
-    Wire.write(M);
-  }else if (M == 0) {
-    M = 1;
-    Wire.write(M);
-  }
-  //Serial.print("SLAVE ENVIA: ");
-  //Serial.println(M);
-  
-}
-
-void luz(){
-  if (M == 1) digitalWrite(13,HIGH);
-  else if (M == 0) digitalWrite(13,LOW);
+void loop() {
+  if (m == 'E') digitalWrite(13, HIGH);
+  else digitalWrite(13, LOW);
+  delay(100);
 }
